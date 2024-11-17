6bc8b77aa068b3e06e78b4b4fc51e4abef1fdb1d
Kenifvt <40598902@live.napier.ac.uk>
2024-11-17 17:13:45 +0000

    Added HTML Pages with Login and Signup

--- hello.py
+++ hello.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, abort
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return '<h1>Hello there</h1>'
+def hello():
+    return '<h1>Hello World</h1>'
+
+@app.route('/forc404')
+def force404():
+    abort(404)
+
+@app.errorhandler(404)
+def pag_not_found(error):
+    return "Couldn't find the page you requested.", 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
